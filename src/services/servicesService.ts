6308b68329f23375d5b7f13d21321b3018c765d9
Fernando Guerreiro <fernando.rodrigues@prodap.com.br>
2025-11-27 09:26:21 -0300

    fix: update deleteService API endpoint to use relative URL

--- src/services/servicesService.ts
+++ src/services/servicesService.ts
@@ -1,5 +1,5 @@
 import { api } from "./api";
 
 export async function deleteService(serviceId: string) {
-  return await api.delete(`http://localhost:3001/services/${serviceId}`);
+  return await api.delete(`/services/${serviceId}`);
 }

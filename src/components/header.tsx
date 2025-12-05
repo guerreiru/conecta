186a7668b1d76191e350b87b9d3702fe6fbf0252
Fernando Guerreiro <fernando.rodrigues@prodap.com.br>
2025-12-04 22:26:42 -0300

    fix: update header link text from "ProMatch" to "ProLocal"

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -8,7 +8,9 @@ export async function Header() {
 
   return (
     <header className="items-center bg-white dark:bg-black-200 justify-between hidden md:flex py-4 px-4 md:px-6 border-b border-gray-200 dark:border-zinc-700">
-      <Link href="/" className="font-bold text-xl">ProMatch</Link>
+      <Link href="/" className="font-bold text-xl">
+        ProLocal
+      </Link>
       <NavHeader accessToken={accessToken} />
     </header>
   );

1eec511727d8a2291d3ada275ffdc017daa599f6
Fernando Guerreiro <fernando.rodrigues@prodap.com.br>
2025-12-05 16:14:31 -0300

    fix: update addressSchema to make stateId and stateName optional

--- src/utils/schemas.ts
+++ src/utils/schemas.ts
@@ -1,14 +1,26 @@
 import z from "zod";
 
-export const addressSchema = z.object({
-  zipCode: z.string().min(8, "CEP é obrigatório"),
-  street: z.string().min(1, "Rua é obrigatória"),
-  number: z.string().min(1, "Número é obrigatório"),
-  neighborhood: z.string().min(1, "Bairro é obrigatório"),
-  stateId: z.string().min(1, "Estado é obrigatório"),
-  stateName: z.string(),
-  cityId: z.string().optional(),
-  cityName: z.string(),
-  website: z.string().optional(),
-  phone: z.string().min(1, "Telefone é obrigatório"),
-});
+export const addressSchema = z
+  .object({
+    zipCode: z.string().min(8, "CEP é obrigatório"),
+    street: z.string().min(1, "Rua é obrigatória"),
+    number: z.string().min(1, "Número é obrigatório"),
+    neighborhood: z.string().min(1, "Bairro é obrigatório"),
+
+    stateId: z.string().optional(),
+    stateName: z.string().optional(),
+
+    cityId: z.string().optional(),
+    cityName: z.string().optional(),
+
+    website: z.string().optional(),
+    phone: z.string().min(1, "Telefone é obrigatório"),
+  })
+  .refine((data) => data.stateId || data.stateName, {
+    message: "Estado é obrigatório",
+    path: ["stateId"],
+  })
+  .refine((data) => data.cityId || data.cityName, {
+    message: "Cidade é obrigatória",
+    path: ["cityId"],
+  });

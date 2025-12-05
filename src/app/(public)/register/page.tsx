301b03acb1ad7694a1589b60fe691d5d5faf95cd
Fernando Guerreiro <fernando.rodrigues@prodap.com.br>
2025-12-04 22:26:34 -0300

    fix: correct formatting and improve readability in Register component

--- src/app/(public)/register/page.tsx
+++ src/app/(public)/register/page.tsx
@@ -6,7 +6,7 @@ import Link from "next/link";
 import React, { useState } from "react";
 
 const Register: React.FC = () => {
-  const [role, setRole] = useState("client")
+  const [role, setRole] = useState("client");
 
   return (
     <div className="flex items-center justify-center pt-6 px-4">
@@ -20,18 +20,24 @@ const Register: React.FC = () => {
           </Link>
         </div>
 
-        <p className="font-semibold text-sm text-center text-zinc-500 dark:text-white">Tipo de conta</p>
+        <p className="font-semibold text-sm text-center text-zinc-500 dark:text-white">
+          Tipo de conta
+        </p>
 
         <div className="bg-gray-100 dark:bg-black grid grid-cols-2 gap-2 p-1 rounded-xl mt-4 mb-7">
           <button
             onClick={() => setRole("client")}
-            className={`text-center rounded-lg cursor-pointer px-4 py-2.5 ${role === 'client' ? 'bg-white dark:bg-black-200' : ''}`}
+            className={`text-center rounded-lg cursor-pointer px-4 py-2.5 ${
+              role === "client" ? "bg-white dark:bg-black-200" : ""
+            }`}
           >
             Cliente
           </button>
           <button
             onClick={() => setRole("professional")}
-            className={`text-center rounded-lg cursor-pointer px-4 py-2.5 ${role === 'professional' ? 'bg-white dark:bg-black-200' : ''}`}
+            className={`text-center rounded-lg cursor-pointer px-4 py-2.5 ${
+              role === "professional" ? "bg-white dark:bg-black-200" : ""
+            }`}
           >
             Profissional
           </button>

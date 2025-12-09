242ad3f8036c02fda2ce64c23a94a7737bad3182
Fernando Guerreiro <fernando.rodrigues@prodap.com.br>
2025-12-08 21:29:01 -0300

    style: improve formatting and readability in HowItWorksCard component

--- src/components/howItWorksCard.tsx
+++ src/components/howItWorksCard.tsx
@@ -11,13 +11,17 @@ type StepProps = {
 function Step({ children, title, description }: StepProps) {
   return (
     <div className="flex flex-col gap-4">
-      <div className="p-2 md:p-3 bg-black w-fit rounded-full self-center">{children}</div>
+      <div className="p-2 md:p-3 bg-black w-fit rounded-full self-center">
+        {children}
+      </div>
 
       <div className="max-w-20 md:max-w-none">
         <p className="text-center text-slate-800 text-sm font-semibold">
           {title}
         </p>
-        <p className="mt-1 text-center text-gray-600 text-[10px]">{description}</p>
+        <p className="mt-1 text-center text-gray-600 dark:text-slate-800 text-[10px]">
+          {description}
+        </p>
       </div>
     </div>
   );
@@ -25,10 +29,10 @@ function Step({ children, title, description }: StepProps) {
 
 export function HowItWorksCard() {
   return (
-    <div className="bg-lime-400 rounded-3xl p-4 text-black grid gap-6">
+    <div className="bg-lime-400/70 rounded-3xl p-4 text-black grid gap-6 my-2">
       <div className="text-center">
         <p className="text-black text-xl font-bold">Como funciona</p>
-        <p className="text-gray-600 text-sm font-medium">
+        <p className="text-gray-600 dark:text-slate-800 text-sm font-medium">
           É muito simples contratar um serviço
         </p>
       </div>

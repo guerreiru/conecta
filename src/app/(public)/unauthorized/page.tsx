84e7cbf62d88999d28dfdd4b5d40ad928a677307
Fernando Guerreiro <fernando.rodrigues@prodap.com.br>
2025-12-15 00:31:58 -0300

    fix: improve layout and accessibility of UnauthorizedPage

--- src/app/(public)/unauthorized/page.tsx
+++ src/app/(public)/unauthorized/page.tsx
@@ -4,14 +4,16 @@ import Link from "next/link";
 export default function UnauthorizedPage() {
   return (
     <main className="min-h-[calc(100vh-65px)] grid place-items-center">
-      <div className="grid gap-4">
-        <h1 className="text-3xl text-center">Não autorizado!</h1>
-        <div>
-          <p>Você não tem permissão para acessar essa área!!!!!!!</p>
+      <div className="grid gap-4 text-center">
+        <h1 className="text-3xl">Não autorizado!</h1>
+
+        <p>Você não tem permissão para acessar essa área!</p>
+
+        <div className="flex justify-center">
+          <Link href="/">
+            <Button>Início</Button>
+          </Link>
         </div>
-        <Link href="/">
-          <Button>Início</Button>
-        </Link>
       </div>
     </main>
   );
